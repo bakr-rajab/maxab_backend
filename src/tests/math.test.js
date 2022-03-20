3e63a91641b477fb53c7f55716b19d12270bc5be
bakr-rajab <abobakr.r.bakry@gmail.com>
2022-03-20 03:22:00 +0200

    async test

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -6,8 +6,16 @@ test("testcase name",()=>{
 
 test("sum function",()=>{
     var sum1 = multiple(2,3)
-    console.log(sum1);
-    if(sum1 !== 6){
-        return  new Error("test case sum hase an error Sum should be 5",sum1)
-    }
+    expect(sum1).toBe(4)
+    // console.log(sum1);
+    // if(sum1 !== 6){
+    //     return  new Error("test case sum hase an error Sum should be 5",sum1)
+    // }
+})
+
+test("async test",async() => {
+    await setTimeout(() =>{
+        expect(1).toBe(1)
+        // done();
+    },2000)
 })
